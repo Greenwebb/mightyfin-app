796a2b4fb0c59e5e383ba51ff5763163ae80676e
br3-mah <nyeleti.bremah@gmail.com>
2024-02-05 17:56:26 +0200

    Mobile App

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/foo', function () {
 //     Artisan::call('storage:link');
 // });
-Route::get('/', WelcomePage::class)->name('welcome');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/share-docs', [UserController::class, 'share_doc'])->name('share.docs');
 
